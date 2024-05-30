d39fa0abb28a2113faecf76f288366cf2ae7bc3b
jsy5888 <oliviasunjin0318@gmail.com>
2024-05-29 20:25:20 -0400

    updated

---
+++ JS/scrpts.js
@@ -0,0 +1,20 @@
+// IIFE
+
+let pokemonRepository = (function(){
+    let pokemonList = [
+    {name :"Bulbasaur", height:7, types: ['grass','posion']},
+    {name :"Charmander", height:6, types: 'fire' },
+    {name :"Squirtle", height:5, types: 'water' }]; //empty array
+    return {
+        add:function(pokemon){
+            pokemonList.push(pokemon);
+            },
+            getAll: function() {
+                return pokemonList;
+            }
+            };
+})();
+
+console.log(pokemonRepository.getAll()); //[]
+pokemonRepository.add({name:'Pikachu'});
+console.log(pokemonRepository.getAll());//[{name:'Pikachu'}]
