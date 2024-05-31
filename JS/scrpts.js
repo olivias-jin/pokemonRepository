021b4b2fc879fe36af37e6464d75941ac07d6555
jsy5888 <oliviasunjin0318@gmail.com>
2024-05-30 20:26:46 -0400

    Update scrpts.js

--- JS/scrpts.js
+++ JS/scrpts.js
@@ -1,57 +1,25 @@
-let pokemonRepository = (function () {
-    let repository = [
-      {
-        name: "Bulbasaur",
-        height: 0.7,
-        types: ["grass", "poison"],
-      },
-      {
-        name: "Charizard",
-        height: 1.7,
-        types: ["fire", "flying"],
-      },
-      {
-        name: "Squirtle",
-        height: 1,
-        types: ["water"],
-      },
-    ];
-  
-    function add(pokemon) {
-      if (
-        typeof pokemon === "object" &&
-        "name" in pokemon &&
-        "height" in pokemon &&
-        "types" in pokemon
-      ) {
-        repository.push(pokemon);
-      } else {
-        console.log("pokemon is not correct");
-      }
-    }
-    function getAll() {
-      return repository;
-    }
-    function addListItem(pokemon){
-      let pokemonList = document.querySelector(".pokemon-list");
-      let listpokemon = document.createElement("li");
-      let button = document.createElement("button");
-      button.innerText = pokemon.name;
-      button.classList.add("button-class");
-      listpokemon.appendChild(button);
-      pokemonList.appendChild(listpokemon);
-    }
+// IIFE
+
+let pokemonRepository = (function(){
+    let pokemonList = [
+    {name :"Bulbasaur", height:7, types: ['grass','posion']},
+    {name :"Charmander", height:6, types: 'fire' },
+    {name :"Squirtle", height:5, types: 'water' }];
     return {
-      add: add,
-      getAll: getAll,
-      addListItem: addListItem
-    };
-  })();
-  
-  pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
-  
-  console.log(pokemonRepository.getAll());
-  
-  pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
-  });
+        add:function(pokemon){
+            pokemonList.push(pokemon);
+            },
+            getAll: function() {
+                return pokemonList;
+            }
+            };
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name:"Pikachu",height:4,types:'electric'});
+console.log(pokemonRepository.getAll());
+
+
+// pokemonList.forEach(function(user){
+//     console.log(user.name + ' type is ' + user.types);
+// });
