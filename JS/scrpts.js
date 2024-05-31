cb23ad145354240549a08160fe1da4a99b67dfb3
jsy5888 <oliviasunjin0318@gmail.com>
2024-05-30 20:51:10 -0400

    update

--- JS/scrpts.js
+++ JS/scrpts.js
@@ -5,14 +5,37 @@ let pokemonRepository = (function(){
     {name :"Bulbasaur", height:0.7, types: ["grass","posion"]},
     {name :"Charmander", height:0.6, types: ["fire"] },
     {name :"Squirtle", height:0.5, types: ["water"] }];
-    return {
-        add:function(pokemon){
-            pokemonList.push(pokemon);
-            },
-            getAll: function() {
-                return pokemonList;
-            }
-            };
+    
+    
+   function add(pokemon) {
+    if (
+        typeof pokemon === "object" &&
+        "name" in pokemon &&
+        "height" in pokemon &&
+        "types" in pokemon
+    ){
+        pokemonRepository.push(pokemon);
+    }else {
+        console.log("pokemon is not corret");
+    }
+   }
+   function getAll(){
+    return pokemonRepository;
+   }
+   function addListItem(pokemon){    
+let pokemonList = document.querySelector(".pokemon-list");
+   let listpokemon = document.createElement("li");
+   let button = document.createElement("button");
+   button.innerText = pokemon.name;
+   button.classList.add("button-class")
+   listpokemon.appendChild(button);
+   pokemonList.appendChild(listpokemon);
+
+   }
+    return{
+        add: add,
+        getAll: getAll,
+    addListItem: addListItem,   };
 })();
 
 console.log(pokemonRepository.getAll());
@@ -20,13 +43,8 @@ pokemonRepository.add({name:"Pikachu",height:0.4,types:["electric"]});
 console.log(pokemonRepository.getAll());
 
 pokemonRepository.getAll().forEach(function(pokemon){
-    let pokemonList = document.querySelector(".pokemon-list");
-    let listpokemon = document.createElement("li");
-    let button = document.createElement("button");
-    button.innerText = "placeholder";
-    button.classList.add("button-class")
-    listpokemon.appendChild(button);
-    pokemonList.appendChild(listpokemon);
+
+    pokemonRepository.addListItem(pokemon);
 });
 
 // pokemonList.forEach(function(user){
