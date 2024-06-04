4a384b3ec921d5186e5d207b3401dc328ba4c80b
jsy5888 <oliviasunjin0318@gmail.com>
2024-06-04 18:25:58 -0400

    Update scrpts.js

--- JS/scrpts.js
+++ JS/scrpts.js
@@ -51,6 +51,7 @@ let pokemonList = document.querySelector(".pokemon-list");
                 detailsUrl: item.url
             };
             add(pokemon);
+            console.log(pokemon);
         });
     }).catch(function(e){
         console.error(e);
