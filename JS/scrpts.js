5e696fbbb0b6596bec595df15cdd47240a0ce12a
jsy5888 <oliviasunjin0318@gmail.com>
2024-06-06 19:41:13 -0400

    Update scrpts.js

--- JS/scrpts.js
+++ JS/scrpts.js
@@ -22,8 +22,10 @@ let pokemonRepository = (function(){
 //    function showDetails(pokemon){
 //     console.log(pokemon)
 //    }
+
+
    function addListItem(pokemon){    
-let pokemonList = document.querySelector(".pokemon-list");
+    let pokemonList = document.querySelector(".pokemon-list");
    let listpokemon = document.createElement("li");
    let button = document.createElement("button");
    button.innerText = pokemon.name;
@@ -94,7 +96,7 @@ loadDetails(pokemon).then(function () {
 
 pokemonRepository.loadList().then(function() {
 pokemonRepository.getAll().forEach(function(pokemon){
-    pokemonRepository.addListItem(pokemon);
+pokemonRepository.addListItem(pokemon);
 });
 });
 
@@ -102,3 +104,46 @@ pokemonRepository.getAll().forEach(function(pokemon){
 // pokemonList.forEach(function(user){
 //     console.log(user.name + ' type is ' + user.types);
 // });
+
+function showModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.add('is-visible');
+  }
+  
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal();
+  });
+
+function showModal(title, text) {
+    let modalContainer = document.querySelector('#modal-container');
+  
+    // Clear all existing modal content
+    modalContainer.innerHTML = '';
+  
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+  
+    // Add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+  
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+  
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+  
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+  
+  
+  
+    modalContainer.classList.add('is-visible');
+  }
+  
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Modal title', 'This is the modal content!');
+  });
